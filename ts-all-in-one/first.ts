0902841bf026c888391c333aa90d334891457598
drow724 <drow724@naver.com>
2024-08-18 20:59:43 +0900

    오버로딩

--- ts-all-in-one/first.ts
+++ ts-all-in-one/first.ts
@@ -651,14 +651,44 @@
 // const b: B = a;
 // const c: typeof a = b;
 
-function add(x: number, y: number): number;
-function add(x: string, y: string): string;
-function add(x: number | string, y: number | string): number | string {
-  return x + y;
+// function add(x: number, y: number): number;
+// function add(x: string, y: string): string;
+// function add(x: number | string, y: number | string): number | string {
+//   return x + y;
+// }
+
+// let a: string | number;
+
+// if (typeof a === "string") {
+//   a.
+// }
+
+declare function add(x: number, y: number): number;
+//declare function add(x: number, y: number, z?: number): number;
+declare function add(x: number, y: number, z: number): number;
+declare function add(x: string, y: string): string;
+
+add(1, 2);
+add(2, 3, 4);
+add("1", "2");
+
+interface Add {
+  (x: number, y: number): number;
+  (x: string, y: string): string;
 }
 
-let a: string | number;
+const add: Add = (x: any, y: any) => x + y;
 
-if (typeof a === "string") {
-  a.
+add(1, 2);
+add(2, 3, 4);
+add("1", 2);
+
+class A {
+  add(x: number, y: number): number;
+  add(x: string, y: string): string;
+  add(x: any, y: any) {
+    return x + y;
+  }
 }
+
+const c = new A().add("1", 2);
